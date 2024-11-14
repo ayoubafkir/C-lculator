c9c44563da269650e01f85dac3d227f0e6388cdd
ayoubafkir <a.afkir97@gmail.com>
2024-11-14 18:45:09 +0100

    first commit

---
+++ Calculator /Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Calculator
+{
+    class MyCalculator
+    {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Simple Calculator");
+        Console.WriteLine("-----------------");
+        
+        // Ask user for first number 
+        Console.Write("Enter the first number: ");
+        double firstNumber = Convert.ToDouble(Console.ReadLine());
+        
+        // Ask the user for an operator
+        Console.Write("Enter an operator (+, -, *, /): ");
+        char op = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+        
+        // Ask the user for a second number 
+        Console.Write("Enter a second number: ");
+        double secondNumber = Convert.ToDouble(Console.ReadLine());
+        
+        // Perform Calculation
+        double result = 0;
+        bool validOperation = true;
+
+        switch (op)
+        {
+            case '+': 
+                result = firstNumber + secondNumber;
+                break;
+            case '-':
+                result = firstNumber - secondNumber;
+                break;
+            case '*':
+                result = firstNumber * secondNumber;
+                break;
+            case '/':
+                if (secondNumber != 0)
+                {
+                    result = firstNumber / secondNumber;
+                }
+                else
+                {
+                    Console.WriteLine("Error: Cannot divide by 0");
+                    validOperation = false;
+                }
+
+                break;
+            default:
+                Console.WriteLine("Error Invalid operator");
+                validOperation = false;
+                break;
+        }
+        
+        // Display results
+        if (validOperation) {
+            Console.WriteLine($"Result:{firstNumber} + {secondNumber} = {result}");
+        }
+
+        Console.WriteLine("Press any ket to exit...");
+        Console.ReadKey();
+
+    }
+    }
+}
+
